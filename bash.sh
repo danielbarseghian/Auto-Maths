8192470bd93521f446469a4c27ab44df3f855ce1
Daniel Barseghian <barseghian.daniel@gmail.com>
2026-03-31 19:00:19 +0200

    Made bash.sh fully functionable

--- bash.sh
+++ bash.sh
@@ -1,3 +1,22 @@
-source venv/bin/activate
+#!/bin/bash
+
+# 1. Create venv if it doesn't exist
+if [ -d "venv" ]; then
+    source venv/bin/activate
+else
+    python3 -m venv venv
+    source venv/bin/activate
+    
+    # Only create .env if it doesn't exist already
+    if [ ! -f ".env" ]; then
+        echo 'key="PAST YOUR KEY HERE"' > .env
+    fi
+    
+    python3 -m pip install -r requirements.txt
+fi
+
+# 2. Run the application
 python3 main.py
+
+# 3. Exit the virtual environment
 deactivate
